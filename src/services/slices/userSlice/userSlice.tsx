7904ff4aca52c15bab4100e7e71caeb1e1fce8ec
Nastpavlova <nastpavlovax@mail.ru>
2025-10-03 12:59:28 +0300

    add async thunk registration user

--- src/services/slices/userSlice/userSlice.tsx
+++ src/services/slices/userSlice/userSlice.tsx
@@ -1,13 +1,20 @@
 import { TUser } from '../../../utils/types';
 import { AppDispatch, RootState } from '../../store';
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import {
-  registerUserApi,
-  loginUserApi,
-  getUserApi,
-  logoutApi,
-  updateUserApi
+import { createSlice, PayloadAction, createAsyncThunk, isRejectedWithValue} from '@reduxjs/toolkit';
+import { setCookie } from '../../../utils/cookie';
+import { 
+  registerUserApi, 
+  loginUserApi, 
+  getUserApi, 
+  logoutApi, 
+  updateUserApi,
+  TAuthResponse,
+  TLoginData,
+  TLogoutResponse,
+  TUserResponse,
+  TRegisterData
 } from '../../../utils/burger-api';
+
 
 interface TUserState {
   isAuthChecked: boolean;
@@ -20,6 +27,67 @@ const initialState: TUserState = {
   user: null,
   error: null
 };
+
+// залогиниться
+export const fetchLogin = createAsyncThunk<
+  TAuthResponse,
+  TLoginData,
+  { rejectValue: string }
+>('user/login', async (data, { rejectWithValue }) => {
+  try {
+    const result = await loginUserApi(data);
+    localStorage.setItem('refreshToken', result.refreshToken);
+    setCookie('accessToken', result.accessToken);
+    return result;
+  } catch (error) {
+    return rejectWithValue((error as Error).message);
+  }
+})
+
+// разлогиниться
+export const fetchLogout = createAsyncThunk<
+  TLogoutResponse,
+  void,
+  { rejectValue: string }
+>('user/logout', async (_, { rejectWithValue }) => {
+  try {
+    const result = await logoutApi();
+    localStorage.removeItem('refreshToken');
+    setCookie('accessToken', '', { expires: -1 });
+    return result;
+  } catch (error) {
+    return rejectWithValue((error as Error).message);
+  }
+})
+
+// получить пользователя
+export const fetchGetUser = createAsyncThunk<
+  TUserResponse,
+  void,
+  { rejectValue: string } 
+>('user/getUser', async (_, { rejectWithValue }) => {
+  try {
+    return await getUserApi();
+  } catch (error) {
+    return rejectWithValue((error as Error).message);
+  }
+})
+
+// регистрация нового юзера
+export const fetchRegisterUser = createAsyncThunk<
+  TAuthResponse,
+  TRegisterData, 
+  { rejectValue: string }
+>('user/registration', async (data, { rejectWithValue }) => {
+  try {
+    const result = await registerUserApi(data);
+    localStorage.setItem('refreshToken', result.refreshToken);
+    setCookie('accessToken', result.accessToken);
+    return result;
+  } catch (error) {
+    return rejectWithValue((error as Error).message);
+  }
+})
 
 export const userSlice = createSlice({
   name: 'user',

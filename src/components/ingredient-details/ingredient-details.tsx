1174714034097de30abdc977da895b872c7d85b0
Nastpavlova <nastpavlovax@mail.ru>
2025-10-06 11:54:03 +0300

    add ingredient modal

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,19 +1,14 @@
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
-// import { useParams } from 'react-router-dom';
-// import { useSelector } from '../../services/store';
-// import { selectorIngredients } from '../../services/slices/ingredientSlice/ingredientSlice';
+import { useParams } from 'react-router-dom';
+import { useSelector } from '../../services/store';
+import { selectorIngredients } from '../../services/slices/ingredientSlice/ingredientSlice';
 
 export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
-  // const { id } = useParams<{ id: string }>();
-  // const ingredients = useSelector(selectorIngredients);
-  const ingredientData = null;
-
-  // console.log('ingredients = ', ingredients);
-  // console.log('id = ', id);
-  // console.log('ingredientData = ', ingredientData);
+  const { id } = useParams<{ id: string }>();
+  const ingredients = useSelector(selectorIngredients);
+  const ingredientData = ingredients.filter((item) => item._id === id)[0];
 
   if (!ingredientData) {
     return <Preloader />;
@@ -21,12 +16,3 @@ export const IngredientDetails: FC = () => {
 
   return <IngredientDetailsUI ingredientData={ingredientData} />;
 };
-
-//   const ingredientData = ingredients.find((item) => item._id === id);
-
-//   if (!ingredientData) {
-//     return <Preloader />;
-//   }
-
-//   return <IngredientDetailsUI ingredientData={ingredientData} />;
-// };

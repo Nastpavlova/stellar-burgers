2ae951477c34cf38404681bd188c8b7763d227b9
Nastpavlova <nastpavlovax@mail.ru>
2025-10-06 11:30:20 +0300

    added modal functionality

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,5 @@
-import { ConstructorPage } from '@pages';
-import '../../index.css';
-import styles from './app.module.css';
-import { AppHeader } from '@components';
-import { ProtectedRoute } from '../index';
 import {
+  ConstructorPage,
   Feed,
   Login,
   Register,
@@ -12,11 +8,22 @@ import {
   Profile,
   ProfileOrders,
   NotFound404
-} from '../../pages/index';
-import { Routes, Route, Outlet } from 'react-router-dom';
-import { useDispatch, useSelector, RootState } from '../../services/store';
+} from '@pages';
+import '../../index.css';
+import styles from './app.module.css';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import { ProtectedRoute } from '../index';
+import {
+  Routes,
+  Route,
+  Outlet,
+  useLocation,
+  useNavigate
+} from 'react-router-dom';
+import { useDispatch } from '../../services/store';
 import { useEffect } from 'react';
 import { fetchIngredients } from '../../services/slices/ingredientSlice/ingredientSlice';
+import { checkUserAuth } from '../../services/slices/userSlice/userSlice';
 
 function MainPage() {
   return (
@@ -28,95 +35,111 @@ function MainPage() {
 }
 
 function App() {
+  const location = useLocation();
+  const state = location.state as { background?: Location };
+
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const ingredients = useSelector(
-    (state: RootState) => state.ingredients.ingredients
-  );
-  const isLoading = useSelector(
-    (state: RootState) => state.ingredients.isLoading
-  );
+
+  function handleModalClose(): void {
+    navigate(-1);
+  }
 
   useEffect(() => {
     dispatch(fetchIngredients());
+    dispatch(checkUserAuth());
   }, [dispatch]);
 
-  useEffect(() => {}, [ingredients, isLoading]);
-
   return (
-    <Routes>
-      <Route element={<MainPage />}>
-        <Route path='/' element={<ConstructorPage />} />
-        <Route path='/feed' element={<Feed />} />
-        <Route
-          path='/login'
-          element={
-            <ProtectedRoute onlyUnAuth>
-              <Login />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/register'
-          element={
-            <ProtectedRoute onlyUnAuth>
-              <Register />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/forgot-password'
-          element={
-            <ProtectedRoute onlyUnAuth>
-              <ForgotPassword />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/reset-password'
-          element={
-            <ProtectedRoute onlyUnAuth>
-              <ResetPassword />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/profile'
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/profile/orders'
-          element={
-            <ProtectedRoute>
-              <ProfileOrders />
-            </ProtectedRoute>
-          }
-        />
-        <Route path='*' element={<NotFound404 />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route element={<MainPage />}>
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route
+            path='/login'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <Login />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/register'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <Register />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/forgot-password'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <ForgotPassword />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/reset-password'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <ResetPassword />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/profile'
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/profile/orders'
+            element={
+              <ProtectedRoute>
+                <ProfileOrders />
+              </ProtectedRoute>
+            }
+          />
+          <Route path='*' element={<NotFound404 />} />
+        </Route>
+      </Routes>
+
+      {state?.background && (
+        <Routes>
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title={''} onClose={handleModalClose}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title={'Детали ингредиента'} onClose={handleModalClose}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <Modal title={''} onClose={handleModalClose}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+        </Routes>
+      )}
+    </>
   );
 }
 
 export default App;
-
-// {state?.background && (
-//   <Routes>
-//     <Route
-//       path='/profile/orders/:number'
-//       element={
-//         <Modal title={''} onClose={handleModalClose}>
-//           <OrderInfo />
-//         </Modal>
-//       }
-//     />
-//   </Routes>
-
-// Также нужно добавить модалки с дополнительной информацией:
-// по роуту /feed/:number расположите компонент Modal с компонентом OrderInfo;
-// по роуту /ingredients/:id расположите компонент Modal с компонентом IngredientDetails;
-// по защищённому роуту /profile/orders/:number расположите компонент Modal с компонентом OrderInfo .
-// Теперь на главном экране появились компоненты, но пока без данных:
--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,10 +1,19 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+// import { useParams } from 'react-router-dom';
+// import { useSelector } from '../../services/store';
+// import { selectorIngredients } from '../../services/slices/ingredientSlice/ingredientSlice';
 
 export const IngredientDetails: FC = () => {
   /** TODO: взять переменную из стора */
+  // const { id } = useParams<{ id: string }>();
+  // const ingredients = useSelector(selectorIngredients);
   const ingredientData = null;
+
+  // console.log('ingredients = ', ingredients);
+  // console.log('id = ', id);
+  // console.log('ingredientData = ', ingredientData);
 
   if (!ingredientData) {
     return <Preloader />;
@@ -12,3 +21,12 @@ export const IngredientDetails: FC = () => {
 
   return <IngredientDetailsUI ingredientData={ingredientData} />;
 };
+
+//   const ingredientData = ingredients.find((item) => item._id === id);
+
+//   if (!ingredientData) {
+//     return <Preloader />;
+//   }
+
+//   return <IngredientDetailsUI ingredientData={ingredientData} />;
+// };

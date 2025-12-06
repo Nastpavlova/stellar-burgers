b5e1203ced716508c060f3d11369c36b46f2adda
Nastpavlova <Nastpavlovax@mail.ru>
2025-12-06 19:46:39 +0300

    add ordersSlice tests

---
+++ src/services/slices/ordersSlice/orders.mock.ts
@@ -0,0 +1,29 @@
+export const mockOrders = [
+    {
+        _id: '1',
+        status: 'done',
+        name: 'name 1',
+        createdAt: '2025-12-07',
+        updatedAt: '2025-12-07',
+        number: 1,
+        ingredients: ['bun', 'main']
+    },
+    {
+        _id: '2',
+        status: 'pending',
+        name: 'name 2',
+        createdAt: '2025-12-07',
+        updatedAt: '2025-12-07',
+        number: 2,
+        ingredients: ['bun']
+    },
+    {
+        _id: '3',
+        status: 'created',
+        name: 'name 3',
+        createdAt: '2025-12-07',
+        updatedAt: '2025-12-07',
+        number: 3,
+        ingredients: ['bun', 'sause']
+    }
+];

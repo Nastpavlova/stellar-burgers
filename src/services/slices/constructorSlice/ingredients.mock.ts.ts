0b1fa13299f8ac46961c2fc4cac36039eecdf65c
Nastpavlova <Nastpavlovax@mail.ru>
2025-12-06 17:56:02 +0300

    constructorSlice tests add

---
+++ src/services/slices/constructorSlice/ingredients.mock.ts.ts
@@ -0,0 +1,126 @@
+// ingredient - булка
+export const mockBun = {
+    _id: '2',
+    name: 'Краторная булка N-200i',
+    type: 'bun',
+    proteins: 80,
+    fat: 24,
+    carbohydrates: 53,
+    calories: 420,
+    price: 1255,
+    image: 'https://code.s3.yandex.net/react/code/bun-02.png',
+    image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
+    image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png'
+};
+
+// ingredient - ингредиент (type = main)
+export const mockMainIngredient = {
+  _id: '1',
+  name: 'Биокотлета из марсианской Магнолии',
+  type: 'main',
+  proteins: 420,
+  fat: 142,
+  carbohydrates: 242,
+  calories: 4242,
+  price: 424,
+  image: 'https://code.s3.yandex.net/react/code/meat-01.png',
+  image_mobile: 'https://code.s3.yandex.net/react/code/meat-01-mobile.png',
+  image_large: 'https://code.s3.yandex.net/react/code/meat-01-large.png'
+};
+
+// ingredient - соус
+export const mockSauce =   {
+  _id: '3',
+  name: 'Соус с шипами Антарианского плоскоходца',
+  type: 'sauce',
+  proteins: 101,
+  fat: 99,
+  carbohydrates: 100,
+  calories: 100,
+  price: 88,
+  image: 'https://code.s3.yandex.net/react/code/sauce-01.png',
+  image_mobile: 'https://code.s3.yandex.net/react/code/sauce-01-mobile.png',
+  image_large: 'https://code.s3.yandex.net/react/code/sauce-01-large.png'
+};
+
+// ingredients + ID - ингредиенты с id после nanoid для конструктора
+export const mockIngredientsWithID = [
+  {
+    id: 'mockedId-1',
+    _id: '1',
+    name: 'Биокотлета из марсианской Магнолии',
+    type: 'main',
+    proteins: 420,
+    fat: 142,
+    carbohydrates: 242,
+    calories: 4242,
+    price: 424,
+    image: 'https://code.s3.yandex.net/react/code/meat-01.png',
+    image_mobile: 'https://code.s3.yandex.net/react/code/meat-01-mobile.png',
+    image_large: 'https://code.s3.yandex.net/react/code/meat-01-large.png'
+  },
+  {
+    id: 'mockedId-2',
+    _id: '2',
+    name: 'Соус с шипами Антарианского плоскоходца',
+    type: 'sauce',
+    proteins: 101,
+    fat: 99,
+    carbohydrates: 100,
+    calories: 100,
+    price: 88,
+    image: 'https://code.s3.yandex.net/react/code/sauce-01.png',
+    image_mobile: 'https://code.s3.yandex.net/react/code/sauce-01-mobile.png',
+    image_large: 'https://code.s3.yandex.net/react/code/sauce-01-large.png'
+  }
+];
+
+// DELETE: ingredients + ID - ингредиенты с id после удаления 
+export const afterDeleteMockIngredientsWithID = [
+  {
+    id: 'mockedId-2',
+    _id: '2',
+    name: 'Соус с шипами Антарианского плоскоходца',
+    type: 'sauce',
+    proteins: 101,
+    fat: 99,
+    carbohydrates: 100,
+    calories: 100,
+    price: 88,
+    image: 'https://code.s3.yandex.net/react/code/sauce-01.png',
+    image_mobile: 'https://code.s3.yandex.net/react/code/sauce-01-mobile.png',
+    image_large: 'https://code.s3.yandex.net/react/code/sauce-01-large.png'
+  }
+];
+
+// MOVE: ingredients + ID - ингредиенты с id после move 
+export const afterMoveMockIngredientsWithID = [
+  {
+    id: 'mockedId-2',
+    _id: '2',
+    name: 'Соус с шипами Антарианского плоскоходца',
+    type: 'sauce',
+    proteins: 101,
+    fat: 99,
+    carbohydrates: 100,
+    calories: 100,
+    price: 88,
+    image: 'https://code.s3.yandex.net/react/code/sauce-01.png',
+    image_mobile: 'https://code.s3.yandex.net/react/code/sauce-01-mobile.png',
+    image_large: 'https://code.s3.yandex.net/react/code/sauce-01-large.png'
+  },
+  {
+    id: 'mockedId-1',
+    _id: '1',
+    name: 'Биокотлета из марсианской Магнолии',
+    type: 'main',
+    proteins: 420,
+    fat: 142,
+    carbohydrates: 242,
+    calories: 4242,
+    price: 424,
+    image: 'https://code.s3.yandex.net/react/code/meat-01.png',
+    image_mobile: 'https://code.s3.yandex.net/react/code/meat-01-mobile.png',
+    image_large: 'https://code.s3.yandex.net/react/code/meat-01-large.png'
+  }
+];
